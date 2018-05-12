ed4cccdaf8319742d523ad5cc2e2c1ba2c83f2c5
Christopher Prince <chris@SpasticMuffin.biz>
2018-05-12 12:18:39 -0600

    Added last file group test cases.

--- Example/Tests/Client_FileGroup.swift
+++ Example/Tests/Client_FileGroup.swift
@@ -738,7 +738,244 @@ class Client_FileGroup: TestCase {
         waitForExpectations(timeout: 20.0, handler: nil)
     }
     
-    // A group with a version update, appMetaData download, and a deletion.
+    // A download group with a version update, appMetaData download, and a deletion.
+    func testGroupWithVersionUpdateAppMetaDataDownloadAndDeletions() {
+        let fileGroupUUID = UUID().uuidString
+
+        guard let (_, attr1) = uploadSingleFileUsingSync(fileGroupUUID:fileGroupUUID) else {
+            XCTFail()
+            return
+        }
+        
+        guard let (_, attr2) = uploadSingleFileUsingSync(fileGroupUUID:fileGroupUUID) else {
+            XCTFail()
+            return
+        }
+        
+        guard let (_, attr3) = uploadSingleFileUsingSync(fileGroupUUID:fileGroupUUID) else {
+            XCTFail()
+            return
+        }
+        
+        var masterVersion:MasterVersionInt!
+        CoreData.sessionNamed(Constants.coreDataName).performAndWait() {
+            masterVersion = Singleton.get().masterVersion
+        }
+        
+        let uploadFileURL:URL = Bundle(for: TestCase.self).url(forResource: "UploadMe", withExtension: "txt")!
+
+        guard let _ = uploadFile(fileURL:uploadFileURL, mimeType: .text, fileUUID: attr1.fileUUID, serverMasterVersion: masterVersion, fileVersion: 1) else {
+            return
+        }
+        
+        let appMetaData1 = AppMetaData(version: 0, contents: "Foobar")
+        guard uploadAppMetaData(masterVersion: masterVersion, appMetaData: appMetaData1, fileUUID: attr2.fileUUID) else {
+            XCTFail()
+            return
+        }
+        
+        let fileToDelete = ServerAPI.FileToDelete(fileUUID: attr3.fileUUID, fileVersion: 0)
+        uploadDeletion(fileToDelete: fileToDelete, masterVersion: masterVersion)
+        
+        doneUploads(masterVersion: masterVersion, expectedNumberUploads: 3)
+        
+        // Get the download updates with sync
+        
+        let expDone = self.expectation(description: "test1")
+        let versionUpdate = self.expectation(description: "test2")
+        let appMetaData = self.expectation(description: "test3")
+        let deletion = self.expectation(description: "test4")
+        
+        SyncServer.session.eventsDesired = [.syncDone]
+
+        syncServerEventOccurred = {event in
+            switch event {
+            case .syncDone:
+                expDone.fulfill()
+                
+            default:
+                XCTFail("\(event)")
+            }
+        }
+        
+        syncServerFileGroupDownloadComplete = { group in
+            guard group.count == 3 else {
+                XCTFail()
+                return
+            }
+            
+            group.forEach { operation in
+                if operation.attr.fileUUID == attr1.fileUUID {
+                    guard case .file = operation.type else {
+                        XCTFail()
+                        return
+                    }
+                    versionUpdate.fulfill()
+                } else if operation.attr.fileUUID == attr2.fileUUID {
+                    guard case .appMetaData = operation.type else {
+                        XCTFail()
+                        return
+                    }
+                    XCTAssert(operation.attr.appMetaData == appMetaData1.contents, "\(String(describing: operation.attr.appMetaData))")
+                    appMetaData.fulfill()
+                } else if operation.attr.fileUUID == attr3.fileUUID {
+                    guard case .deletion = operation.type else {
+                        XCTFail()
+                        return
+                    }
+                    deletion.fulfill()
+                }
+                else {
+                    XCTFail()
+                }
+            }
+        }
+
+        SyncServer.session.sync()
+        
+        waitForExpectations(timeout: 20.0, handler: nil)
+    }
     
     // Two separate groups with deletions-- nil group UUID in each case.
+    func testTwoGroupsWithDeletionsWithNilGroupId() {
+        guard let (_, attr1) = uploadSingleFileUsingSync() else {
+            XCTFail()
+            return
+        }
+        
+        guard let (_, attr2) = uploadSingleFileUsingSync() else {
+            XCTFail()
+            return
+        }
+        
+        var masterVersion:MasterVersionInt!
+        CoreData.sessionNamed(Constants.coreDataName).performAndWait() {
+            masterVersion = Singleton.get().masterVersion
+        }
+        
+        let fileToDelete1 = ServerAPI.FileToDelete(fileUUID: attr1.fileUUID, fileVersion: 0)
+        uploadDeletion(fileToDelete: fileToDelete1, masterVersion: masterVersion)
+        
+        let fileToDelete2 = ServerAPI.FileToDelete(fileUUID: attr2.fileUUID, fileVersion: 0)
+        uploadDeletion(fileToDelete: fileToDelete2, masterVersion: masterVersion)
+        
+        doneUploads(masterVersion: masterVersion, expectedNumberUploads: 2)
+        
+        // Get the download updates with sync
+        
+        let expDone = self.expectation(description: "test1")
+        let deletion1 = self.expectation(description: "test2")
+        let deletion2 = self.expectation(description: "test3")
+        
+        SyncServer.session.eventsDesired = [.syncDone]
+
+        syncServerEventOccurred = {event in
+            switch event {
+            case .syncDone:
+                expDone.fulfill()
+                
+            default:
+                XCTFail("\(event)")
+            }
+        }
+        
+        syncServerFileGroupDownloadComplete = { group in
+            guard group.count == 1 else {
+                XCTFail()
+                return
+            }
+            
+            guard case .deletion = group[0].type else {
+                XCTFail()
+                return
+            }
+            
+            if group[0].attr.fileUUID == attr1.fileUUID {
+                deletion1.fulfill()
+            }
+            else if group[0].attr.fileUUID == attr2.fileUUID {
+                deletion2.fulfill()
+            }
+            else {
+                XCTFail()
+            }
+            
+            XCTAssert(group[0].attr.fileGroupUUID == nil)
+        }
+
+        SyncServer.session.sync()
+        
+        waitForExpectations(timeout: 20.0, handler: nil)
+    }
+    
+    // A group with a larger numbers of operations-- e.g., with 10 downloads.
+    func testFileGroupWithLargerNumberOfOperations() {
+        // Upload a group of files.
+        let masterVersion = getMasterVersion()
+        
+        let fileGroupUUID = UUID().uuidString
+        let numberOfFiles = 10
+        var fileUUIDs = [String]()
+        
+        for _ in 1...numberOfFiles {
+            let fileUUID = UUID().uuidString
+            fileUUIDs += [fileUUID]
+        }
+
+        let uploadFileURL:URL = Bundle(for: TestCase.self).url(forResource: "UploadMe", withExtension: "txt")!
+
+        for index in 0..<numberOfFiles {
+            let fileUUID = fileUUIDs[index]
+            guard let _ = uploadFile(fileURL:uploadFileURL, mimeType: .text, fileUUID: fileUUID, serverMasterVersion: masterVersion, fileGroupUUID: fileGroupUUID) else {
+                return
+            }
+        }
+        
+        doneUploads(masterVersion: masterVersion, expectedNumberUploads: Int64(numberOfFiles))
+        
+        // Download them with sync
+        
+        let expDone = self.expectation(description: "test1")
+        let groupDone = self.expectation(description: "test2")
+
+        SyncServer.session.eventsDesired = [.syncDone]
+
+        syncServerEventOccurred = {event in
+            switch event {
+            case .syncDone:
+                expDone.fulfill()
+                
+            default:
+                XCTFail("\(event)")
+            }
+        }
+        
+        syncServerFileGroupDownloadComplete = { group in
+            XCTAssert(group.count == numberOfFiles)
+            
+            group.forEach { file in
+                guard case .file = file.type else {
+                    XCTFail()
+                    return
+                }
+                
+                XCTAssert(file.attr.fileGroupUUID == fileGroupUUID)
+                
+                guard let index = (fileUUIDs.index {$0 == file.attr.fileUUID}) else {
+                    XCTFail()
+                    return
+                }
+                
+                fileUUIDs.remove(at: index)
+            }
+            
+            XCTAssert(fileUUIDs.count == 0)
+            
+            groupDone.fulfill()
+        }
+
+        SyncServer.session.sync()
+        
+        waitForExpectations(timeout: 20.0, handler: nil)
+    }
 }
